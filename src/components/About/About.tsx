d5f599bec41f2cd6eb20306bb412452b0703436c
Jayom5797 <jayom5797@gmail.com>
2025-07-05 21:25:57 +0530

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,7 +1,14 @@
-<img
-  src="/assets/portfolio/skills/linux-original.svg"
-  height={100}
-  width={100}
-  style={{ objectFit: 'contain' }}
-  alt="Linux"
-/> 
+import React from 'react';
+
+const About = () => {
+  return (
+    <img
+      src="/assets/portfolio/skills/linux-original.svg"
+      height={100}
+      width={100}
+      alt="Linux Logo"
+    />
+  );
+};
+
+export default About;
