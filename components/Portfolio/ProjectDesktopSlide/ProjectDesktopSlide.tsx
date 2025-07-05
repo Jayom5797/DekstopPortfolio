8e39bb4841d0fe5f4c9aca9e910c4c16f455a469
Jayom5797 <jayom5797@gmail.com>
2025-07-05 20:59:20 +0530

    Remove other unsed imports

--- components/Portfolio/ProjectDesktopSlide/ProjectDesktopSlide.tsx
+++ components/Portfolio/ProjectDesktopSlide/ProjectDesktopSlide.tsx
@@ -1,19 +1,3 @@
-import React, { useEffect, useRef, useState } from 'react';
-import * as Styled from './ProjectDesktopSlide.styles';
-import { IHighlightedProject } from '../../../types/portfolio';
-import Image from 'next/image';
-import SectionHeader from '../Typography/SectionHeader/SectionHeader';
-import PortfolioParagraph from '../Typography/PortfolioParagraph/PortfolioParagraph';
-import ActionButton from '../ActionButton/ActionButton';
-import { FiCast, FiGithub } from 'react-icons/fi';
-import { useIntersectionObserver } from '../../../hooks/useIntersectionObserver';
-
-/**
- *Renders full-viewport project slide
- *@function Slide
- *@param {IHighlightedProject} projectDetails - project details data to be mapped into view
- *@returns {JSX.Element} - Rendered ProjectDesktopSlide component
- */
 const ProjectDesktopSlide = (): JSX.Element => {
   return <></>;
 };
